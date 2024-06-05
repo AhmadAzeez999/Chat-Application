954884a8fde9cc79c1f0fe6bdfc91336cc5ee0ea
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2024-06-05 16:46:19 -0700

    Added system messages and informing the user that the other is typing

--- server/server.js
+++ server/server.js
@@ -2,42 +2,13 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
+const initializeSocket = require('./socket')
 
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,
-{
-    cors:
-    {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-});
-
-io.on("connection", (socket) =>
-{
-    console.log(`User connected: ${socket.id}`);
-
-
-    socket.on("join_room", (room) =>
-    {
-        socket.join(room);
-        console.log(`User with ID: ${socket.id} joined room: ${room}`);
-    });
-
-    socket.on("send_message", (data) =>
-    {
-        socket.to(data.currentRoom).emit("receive_message", data);
-    });
-
-    socket.on("disconnect", () =>
-    {
-        console.log(`User disconnected: ${socket.id}`);
-    });
-});
+initializeSocket(server);
 
 server.listen(5000, () =>
 {
